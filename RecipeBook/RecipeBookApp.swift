67edfe81832ef8234d16cd3cc51f1173313555a6
Kainoa Jim <jimkb@bc.edu>
2022-11-29 19:04:01 -0500

    Initial Commit

---
+++ RecipeBook/RecipeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookApp.swift
+//  RecipeBook
+//
+//  Created by Kainoa Jim on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
